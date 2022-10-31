c261f57551740b83e89985cd81ccac04fedd8bc9
Devineni <rd22aak@herts.ac.uk>
2022-10-31 12:37:07 +0000

    first commit

---
+++ lab_week4.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import clean_up as cl   #thi si the file name clean_up.py
+
+
+
+
+#string = clean("HI THS IS ME...TESTIN500202, @CHECKing the scrip897t")
+#print(string)
+
+
+with open("big_data.txt", 'r')as text:
+    all_words = []
+    count = 0
+    for line in text:
+        words = line.split()
+        count += 1
+        
+        for word in words:
+            word = cl.clean(word)
+            all_words.append(word)
+    print('total count: ',count)
+    print("num of words: ", len(all_words), '\n')
+        
+    
+    
+
+df_words = pd.DataFrame(data =all_words, columns=('words',) )
+count_words = df_words['words'].value_counts()
+print(count_words)
