28fbf30f38101e78fa73098f178b629a3b6b97e9
Devineni <rd22aak@herts.ac.uk>
2022-10-31 12:57:30 +0000

    second commit

--- lab_week4.py
+++ lab_week4.py
@@ -27,3 +27,28 @@ with open("big_data.txt", 'r')as text:
 df_words = pd.DataFrame(data =all_words, columns=('words',) )
 count_words = df_words['words'].value_counts()
 print(count_words)
+
+
+
+with open("big_data.txt", 'r')as text:
+    all_letters = []
+    count = 0
+    for line in text:
+        words = line.split()
+        count += 1
+        
+        for word in words:
+            word = cl.clean(word)
+            #all_words.append(word)
+            letters = list(words)
+            for l in letter:
+                all_letters.append(l)
+    print('total count: ',count)
+    print("num of words: ", len(all_words), '\n')
+        
+    
+    
+
+df_words = pd.DataFrame(data =all_words, columns=('words',) )
+count_words = df_words['words'].value_counts()
+print(count_words)
